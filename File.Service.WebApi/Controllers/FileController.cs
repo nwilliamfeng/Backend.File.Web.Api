df1c3cd8ce45de1bf892af3a0d515d28564a8f55
nwilliamfeng <nwilliamfeng@163.com>
2019-06-13 20:03:25 +0800

    555

---
+++ File.Service.WebApi/Controllers/FileController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace File.Service.WebApi.Controllers
+{
+    public class FileController : ApiController
+    {
+
+     
+
+        [HttpPost]
+        public async Task<IHttpActionResult> Upload()
+        {
+            var httpRequest = HttpContext.Current.Request;
+            var dir = httpRequest.Headers.GetValues("dir").FirstOrDefault();
+            if (httpRequest.Files.Count == 0)
+                return this.JsonResult(false.ToJson("未找到文件"));
+
+            foreach (string file in httpRequest.Files)
+            {
+                var postFile = httpRequest.Files[file];
+                postFile.
+                var serverPath = HttpContext.Current.Server.MapPath("~/" + postFile.FileName);
+                postFile.SaveAs(serverPath);
+            }
+
+        }
+    }
+}
